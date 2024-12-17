91c035c7f586a5402f6e25e487c8799c7a5b2c75
AenuHub <jesuskane92@gmail.com>
2024-12-17 18:25:07 +0300

    update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ window.title("Password Manager")
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
-logo = PhotoImage(file="logo.png")
+logo = PhotoImage(file="images/logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
